cd887cf96d009d6ad88d9438912337ce1319e38c
sienaindigoandlavender <siena@indigoandlavender.love>
2026-02-26 19:39:47 +0000

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -3,6 +3,23 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
+  async headers() {
+    return [
+      {
+        source: '/data/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOW-FROM https://www.slowmorocco.com',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://www.slowmorocco.com https://slowmorocco.com",
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
